1153663f96235e6a869f072e84b2e021f34f80f2
SooyeonCheon123 <suyeon5445@gmail.com>
2023-01-21 21:49:25 -0800

    function

---
+++ whichSchool.js
@@ -0,0 +1,9 @@
+function whichSchool (age){
+  if (age < 13) {
+    console.log("Elementary School");
+  } else if (18>= age >=13) {
+    console.log("Secondary School");
+  } else {
+    console.log("Lightgouse Labs");
+  }
+}
